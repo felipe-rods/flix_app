9087ec2b18c71e569b211fc365240630693bfdc9
Felipe Rodrigues <felipe.git@outlook.com>
2025-02-19 14:19:27 -0300

    Finished Genre application layer

--- genres/page.py
+++ genres/page.py
@@ -7,23 +7,21 @@ from genres.service import GenreService
 def show_genres():
     genre_service = GenreService()
     genres = genre_service.get_genres()
-
     if genres:
         st.write('Genres List:')
-        genres_df = pd.json_normalize(genres)
+        genres_pd = pd.json_normalize(genres)
         AgGrid(
-            data=genres_df,
-            reload_data=True,
+            data=genres_pd,
             key='genres_grid',
         )
     else:
-        st.warning('No genres found.')
-
-    st.title('New Genre')
+        st.warning('No genre found.')
+    
+    st.title('New Genre:')
     name = st.text_input('Genre')
     if st.button('Register'):
         new_genre = genre_service.create_genre(
-            name=name,
+            name=name
         )
         if new_genre:
             st.rerun()
