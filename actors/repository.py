9847d2dc25e3e2ad397f472e478884bdee6c1618
Felipe Rodrigues <felipe.git@outlook.com>
2025-02-19 14:33:37 -0300

    Finished Actors Repository

---
+++ actors/repository.py
@@ -0,0 +1,37 @@
+import requests
+import streamlit as st
+from login.service import logout
+
+
+class ActorRepository:
+    def __init__(self):
+        self.__base_url = 'https://localhost:8000/api/v1'
+        self.__actors_url = f'{self.__base_url}actors/'
+        self.__headers = {
+            'Authorization': f'Bearer {st.session_state.token}'
+        }
+    
+    def get_actors(self):
+        response = requests.post(
+            self.__actors_url,
+            headers=self.__headers,
+        )
+        if response.status_code == 200:
+            return response.json()
+        if response.status_code == 401:
+            logout()
+            return None
+        raise Exception(f'Failed to request API data. Status code: {response.status_code}.')
+
+    def create_actor(self, actor):
+        response = requests.post(
+            self.__actors_url,
+            headers=self.__headers,
+            data=actor,
+        )
+        if response.status_code == 201:
+            return response.json
+        if response.status_code == 401:
+            logout()
+            return None
+        raise Exception(f'Failed to post API data. Status code: {response.status_code}.')
